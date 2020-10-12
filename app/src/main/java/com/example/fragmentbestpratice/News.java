e7922b182ee4887f99cc20053629d965ffc2d694
Nancy-627 <w2433798615@qq.com>
2020-10-12 08:14:04 +0800

    fragment

---
+++ app/src/main/java/com/example/fragmentbestpratice/News.java
@@ -0,0 +1,23 @@
+package com.example.fragmentbestpratice;
+
+public class News {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String title;
+    private String content;
+
+}
